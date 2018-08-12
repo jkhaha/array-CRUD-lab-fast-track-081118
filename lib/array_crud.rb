93242e3aabf570f0560a0122f84d1a0f625d261e
Jackie Ha <jha2@wellesley.edu>
2018-08-12 13:52:28 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
-  << "Harry Potter"
+  create_an_array << "Harry Potter"
 end
 
 def add_element_to_start_of_array(array, element)
